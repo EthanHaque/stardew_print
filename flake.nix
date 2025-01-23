fca8f9706a811f885059a2d70d643adfe7aac4c7
Ethan Haque <ethan.k.haque@gmail.com>
2025-01-23 02:44:55 -0500

    feat(flake) use devenv for nix develop configuration

--- flake.nix
+++ flake.nix
@@ -1,36 +1,57 @@
 {
   inputs = {
+    nixpkgs.url = "github:cachix/devenv-nixpkgs/rolling";
     systems.url = "github:nix-systems/default";
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    devenv.url = "github:cachix/devenv";
+    devenv.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs-python.url = "github:cachix/nixpkgs-python";
   };
 
-  outputs =
-  {
-    self,
-    nixpkgs,
-    systems,
-  }:
-  let
-    forEachSystem =
-      f: nixpkgs.lib.genAttrs (import systems) (system: f { pkgs = import nixpkgs { inherit system; }; });
-  in
-  {
-    devShells = forEachSystem (
-      { pkgs }:
-      {
-        default =
-        let
-          python = pkgs.python312;
-        in
-        pkgs.mkShellNoCC {
-          packages = with pkgs; [
-            uv
-            (python.withPackages (ps: with ps;
-            [
-            ]))
-          ];
-        };
-      }
-    );
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
   };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+      {
+      packages = forEachSystem (system: {
+        devenv-up = self.devShells.${system}.default.config.procfileScript;
+        devenv-test = self.devShells.${system}.default.config.test;
+      });
+
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+            {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  packages = with pkgs; [
+                    pre-commit
+                    (pkgs.python312.withPackages (ps: with ps;
+                      [
+                        numpy
+                        pillow
+                      ]))
+                  ];
+
+                  languages.python = {
+                    enable = true;
+                    version = "3.12.8";
+                    venv = {
+                      enable = true;
+                      quiet = true;
+                    };
+                  };
+                }
+              ];
+            };
+          });
+    };
 }
